b1c40145c42ec15e43cc08cdd2c9b0602191c965
oopfan <oopfan@optonline.net>
2019-09-05 11:00:05 -0400

    Instructions image dialog.

--- src/pages/color-balance-instructions/color-balance-instructions.js
+++ src/pages/color-balance-instructions/color-balance-instructions.js
@@ -1,10 +1,24 @@
-define(['knockout', 'scroll-view', 'text!./color-balance-instructions.html'], function(ko, scrollView, templateMarkup) {
+define(['knockout', 'ko-modal-helper', 'scroll-view', 'text!./color-balance-instructions.html'], function(ko, modalHelper, scrollView, templateMarkup) {
 
   function ColorBalanceInstructions() {
     this.scrollTo = function(data, event) {
       var id = $(event.target).attr('href');
       scrollView(id);
-    }
+    };
+    this.superSizeIt = function(data, event) {
+      var imgSrc = $(event.target).attr('src');
+      var modalTitle = $(event.target).attr("alt");
+      modalHelper.showModal({
+        viewModel: new ModalViewModel(imgSrc, modalTitle),
+        context: this
+      });
+    };
+  }
+
+  function ModalViewModel(imgSrc, modalTitle) {
+    this.template = "modal-img-full";
+    this.imgSrc = ko.observable(imgSrc);
+    this.modalTitle = ko.observable(modalTitle);
   }
 
   ColorBalanceInstructions.prototype.dispose = function() {};
